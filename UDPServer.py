130edf847ef13ae6a60eff8bde23c51165bffd97
amogh-2007 <PES1UG24CS653@stu.pes.edu>
2026-03-16 22:21:48 +0530

    Update UDPServer.py

--- UDPServer.py
+++ UDPServer.py
@@ -10,7 +10,6 @@ UDPServerSocket.bind((localIP, localPort))
 
 print("UDP server up and listening")
 
-# Queue size
 BATCH_SIZE = 5
 log_queue = Queue(maxsize=BATCH_SIZE)
 
@@ -19,25 +18,17 @@ while True:
     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
     message = bytesAddressPair[0].decode()
 
-    # If queue not full → add log
     if not log_queue.full():
         log_queue.put(message)
-        print("Log buffered:", message)
 
-    # If queue full → process logs
     if log_queue.full():
-
-        print("\n---- Processing 5 Logs ----")
 
         logs = []
 
         while not log_queue.empty():
             logs.append(log_queue.get())
 
-        # Time ordering
         logs.sort()
 
         for log in logs:
             print(log)
-
-        print("---- Queue cleared ----\n")
